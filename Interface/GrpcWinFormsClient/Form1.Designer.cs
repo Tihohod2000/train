3c1151d9fe9d2b3c2ebec9c3461e15f0bc38135a
Tihohod2000 <dumanovskiy15@gmail.com>
2025-01-25 13:37:07 +0300

    Update3

---
+++ Interface/GrpcWinFormsClient/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace GrpcWinFormsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStartDate = new System.Windows.Forms.TextBox();
+            this.txtEndDate = new System.Windows.Forms.TextBox();
+            this.btnFetchData = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtStartDate
+            // 
+            this.txtStartDate.Location = new System.Drawing.Point(40, 22);
+            this.txtStartDate.Name = "txtStartDate";
+            this.txtStartDate.Size = new System.Drawing.Size(100, 20);
+            this.txtStartDate.TabIndex = 0;
+            // 
+            // txtEndDate
+            // 
+            this.txtEndDate.Location = new System.Drawing.Point(165, 22);
+            this.txtEndDate.Name = "txtEndDate";
+            this.txtEndDate.Size = new System.Drawing.Size(100, 20);
+            this.txtEndDate.TabIndex = 1;
+            // 
+            // btnFetchData
+            // 
+            this.btnFetchData.Location = new System.Drawing.Point(126, 67);
+            this.btnFetchData.Name = "btnFetchData";
+            this.btnFetchData.Size = new System.Drawing.Size(75, 23);
+            this.btnFetchData.TabIndex = 2;
+            this.btnFetchData.Text = "button1";
+            this.btnFetchData.UseVisualStyleBackColor = true;
+            this.btnFetchData.Click += new System.EventHandler(this.btnFetchData_Click_1);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(101, 158);
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(100, 20);
+            this.txtResponse.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.btnFetchData);
+            this.Controls.Add(this.txtEndDate);
+            this.Controls.Add(this.txtStartDate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStartDate;
+        private System.Windows.Forms.TextBox txtEndDate;
+        private System.Windows.Forms.Button btnFetchData;
+        private System.Windows.Forms.TextBox txtResponse;
+    }
+}
+
---
+++ Interface/GrpcWinFormsClient/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+using Grpc.Net.Client;
+using GrpcStationService;
+using Google.Protobuf.WellKnownTypes; // Для работы с Timestamp
+using System.Threading.Tasks;
+using Grpc.Core;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace GrpcWinFormsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnFetchData_Click_1(object sender, EventArgs e)
+        {
+            // Получаем введенные данные
+            
+
+            /* // Проверяем корректность дат
+             if (!DateTime.TryParse(dateStart, out DateTime startDate) ||
+                 !DateTime.TryParse(dateEnd, out DateTime endDate))
+             {
+                 MessageBox.Show("Пожалуйста, введите корректные даты в формате YYYY-MM-DD.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                 return;
+             }*/
+
+            // Преобразуем DateTime в Timestamp
+            try
+            {
+                // Создаём канал и клиента
+                using var channel = GrpcChannel.ForAddress("http://localhost:5033");
+                var client = new WagonService.WagonServiceClient(channel);
+
+                string dateStart = txtStartDate.Text;
+                string dateEnd = txtEndDate.Text;
+
+                var startDate = DateTime.Parse(dateStart).ToUniversalTime();
+                var endDate = DateTime.Parse(dateEnd).ToUniversalTime();
+
+                // Преобразуем DateTime в Timestamp
+                var startTimestamp = Timestamp.FromDateTime(startDate);
+                var endTimestamp = Timestamp.FromDateTime(endDate);
+
+                // Создаём запрос
+                var wagonRequest = new WagonRequest
+                {
+                    DateStart = startTimestamp,
+                    DateEnd = endTimestamp
+                };
+
+                // Отправляем запрос
+                var response = await client.GetWagonsByDateAsync(wagonRequest).ConfigureAwait(false);
+
+                Console.WriteLine("Ответ: " + response);
+
+                // Обрабатываем ответ
+                txtResponse.Clear();
+                foreach (var wagon in response.Wagons)
+                {
+                    txtResponse.AppendText($"Инвентарный номер: {wagon.InventoryNumber}\n");
+                    txtResponse.AppendText($"Время прибытия: {wagon.ArrivalTime}\n");
+                    txtResponse.AppendText($"Время отправления: {wagon.DepartureTime}\n");
+                    txtResponse.AppendText("\n");
+                }
+            }
+            catch (RpcException ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Status.Detail}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
